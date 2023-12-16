9f7131c38f9b112e3fa7360200348dded0b96dd7
jeh7sx <jerryleonardo84@gmail.com>
2023-12-16 11:49:13 -0300

    ...

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ $('#WAButton').floatingWhatsApp({
     popupMessage: 'Deixe uma Mensagem', //Popup Message
     showPopup: true, //Enables popup display
      //buttonImage: '<img src="./img/whatsapp.svg"/>', //Button Image
-    headerColor: 'cadetblue', //Custom header color
-    backgroundColor: '#fff', //Custom background button color
+    headerColor: 'green', //Custom header color
+    backgroundColor: 'green', //Custom background button color
     position: "right", //Position: left | right
     size: 72,
     zIndex: 10
