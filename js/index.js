7f2c2597c0734b1689d32dc7799fe37c1b8f03c5
jeh7sx <jerryleonardo84@gmail.com>
2023-12-16 08:49:01 -0300

    WhatsApp
    Botão flutuante de WhatsApp adicionado em todas as paginas.
    
    index.js criado e script adicionado em todas paginas.

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('#WAButton').floatingWhatsApp({
+    phone: 'seu-numero-no-formato-internacional', //WhatsApp phone number sem espaco e letra
+    headerTitle: 'Chat' , //Popup Title
+    popupMessage: 'Deixe uma Mensagem', //Popup Message
+    showPopup: true, //Enables popup display
+     //buttonImage: '<img src="./img/whatsapp.svg"/>', //Button Image
+    headerColor: 'cadetblue', //Custom header color
+    backgroundColor: '#fff', //Custom background button color
+    position: "right", //Position: left | right
+    size: 72,
+    zIndex: 10
+ });
