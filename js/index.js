233214595189880b798a5da801b860b42412e7a3
jeh7sx <jerryleonardo84@gmail.com>
2023-12-16 14:56:53 -0300

    ...

--- js/index.js
+++ js/index.js
@@ -10,3 +10,18 @@ $('#WAButton').floatingWhatsApp({
     size: 72,
     zIndex: 10
  });
+
+
+ $(function() {
+
+    // SETUP
+    var $span ;
+                                    // item is an empty string
+    $span = $(' span , p');                               // Cache the unordered list
+              // Cache form to add new items
+            // Cache button to show form
+  
+    $('span, p').hide().each(function(index) {          // Hide list items
+      $(this).delay(450 * index).fadeIn(1600);     // Then fade them in
+    });
+});
